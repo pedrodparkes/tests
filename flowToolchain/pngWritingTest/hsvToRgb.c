98a06431a23e8ad7bf6b2430a76311505f68a17a
Alex Servirog <servalex1@gmail.com>
2016-08-11 02:07:50 +0300

    fixed hsv to rgb conversion

--- flowToolchain/pngWritingTest/hsvToRgb.c
+++ flowToolchain/pngWritingTest/hsvToRgb.c
@@ -1,13 +1,7 @@
 #include "hsvToRgb.h"
 #include <math.h>
 #include "stdio.h"
-
-int abs(int a)
-{
-	if(a>0)
-		return a;
-	return -a;
-}
+#include "stdlib.h"
 
 int between_ne(float arg, int min, int max)
 {
@@ -16,53 +10,76 @@ int between_ne(float arg, int min, int max)
 
 void hsvToRgb(png_bytep rgbp, float h, float s, float v)
 {
+	int debug = 0;
+	if(debug)
+		printf("h s v \t%f\t%f\t%f\n", h, s, v);
 	float c = v*s;
 
-	float x = c*(1 - abs(h/60)%2 - 1);
+	float x = c*(1 - fabs(fmodf(h/60.f,2.f) - 1));
 	float m = v-c;	
 
 	int r, g, b;
 
+	if(debug)
+		printf("x c m \t%f\t%f\t%f\n", x, c, m);
+
 	if(between_ne(h, 0, 60))
 	{
+		if(debug)
+			printf("0-60\n");
 		r = (int)ceil((c+m)*255);
 		g = (int)ceil((x+m)*255);
 		b = (int)ceil((0+m)*255);
 	}
 	else if(between_ne(h, 60, 120))
 	{
+		if(debug)
+			printf("60-120\n");
 		r = (int)ceil((x+m)*255);
 		g = (int)ceil((c+m)*255);
 		b = (int)ceil((0+m)*255);
 	}
 	else if(between_ne(h, 120, 180))
 	{
+		if(debug)
+			printf("120-180\n");
 		r = (int)ceil((0+m)*255);
 		g = (int)ceil((c+m)*255);
 		b = (int)ceil((x+m)*255);
 	}
 	else if(between_ne(h, 180, 240))
 	{
+		if(debug)
+			printf("180-240\n");
 		r = (int)ceil((0+m)*255);
 		g = (int)ceil((x+m)*255);
 		b = (int)ceil((c+m)*255);
 	}
 	else if(between_ne(h, 240, 300))
 	{
+		if(debug)
+			printf("240-300\n");
 		r = (int)ceil((x+m)*255);
 		g = (int)ceil((0+m)*255);
 		b = (int)ceil((c+m)*255);
 	}
 	else if(between_ne(h, 300, 360))
 	{
+		if(debug)
+			printf("300-360\n");
 		r = (int)ceil((c+m)*255);
 		g = (int)ceil((0+m)*255);
 		b = (int)ceil((x+m)*255);
 	}
 
+	if(debug)
+		printf("output \t%d\t%d\t%d\n", r, g, b);
 
 	rgbp[0] = r;
 	rgbp[1] = g;
 	rgbp[2] = b;
+
+	if(debug)
+		printf("output2 \t%d\t%d\t%d\n", rgbp[0], rgbp[1], rgbp[2]);
 }
 
--- flowToolchain/pngWritingTest/main.c
+++ flowToolchain/pngWritingTest/main.c
@@ -1,12 +1,16 @@
 #include "writePng.h"
 #include "hsvToRgb.h"
+#include "math.h"
+#include "stdlib.h"
+#include "stdio.h"
 
-int main(int argc, char* argv)
+int main(int argc, char** argv)
 {
-	png_bytep p = (png_bytep)malloc(3*sizeof(png_byte));
-	hsvToRgb(p, 162, 1, 1);
-	printf("%f %f %f", (float)p[0], p[1], p[2]);
 /*
+	png_bytep p = (png_bytep)malloc(3*sizeof(png_byte));
+	hsvToRgb(p, atof(argv[1]), atof(argv[2]), atof(argv[3]));
+	printf("final result %d %d %d\n", p[0], p[1], p[2]);
+*/
 	int width = 360;
 	int height = 100;
 	float* hue = malloc(width*height*sizeof(float));
@@ -16,7 +20,7 @@ int main(int argc, char* argv)
 	for(int j = 0; j<height; j++)
 		for(int i = 0; i<width; i++)
 		{
-			hue[j*width+i] = (float)i/(float)width/3;
+			hue[j*width+i] = (float)i/(float)width;
 			saturation[j*width+i] = (float)j/(float)height;
 		}
 
@@ -26,6 +30,5 @@ int main(int argc, char* argv)
 	// free memory
 	free(hue);
 	free(saturation);
-*/
 	return 0;
 }
