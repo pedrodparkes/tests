606b3d1c436cdacd43a626faba04c2b2b6ede4bf
checkatillo <checkatillo@gmail.com>
2016-08-24 19:20:02 +0300

    Added cygwin install cmd script
    Put bat file alongside the setup-x86_64.exe of cygwin and run it on windows to install minimal needed cygwin environment

---
+++ cygwin-install.bat
@@ -0,0 +1,34 @@
+@ECHO OFF
+REM -- Automates cygwin installation
+
+SETLOCAL
+
+REM -- Change to the directory of the executing batch file
+CD %~dp0
+
+REM -- Configure our paths
+SET SITE=http://mirrors.kernel.org/sourceware/cygwin/
+SET LOCALDIR=%LOCALAPPDATA%/cygwin
+SET ROOTDIR=C:/cygwin64
+
+REM -- These are the packages we will install (in addition to the default packages)
+SET PACKAGES=mintty,wget,ctags,diffutils,git,git-completion,git-svn,mercurial
+SET PACKAGES=%PACKAGES%,gcc-core,make,automake,autoconf,readline,libncursesw-devel,libiconv,zlib-devel,gettext
+SET PACKAGES=%PACKAGES%,lua,python,ruby
+SET PACKAGES=%PACKAGES%,vim,nano,gcc-g++
+
+REM -- Do it!
+ECHO *** INSTALLING PACKAGES
+setup-x86_64 -q -D -L -d -g -o -s %SITE% -l "%LOCALDIR%" -R "%ROOTDIR%" -C Base -P %PACKAGES%
+
+REM -- Show what we did
+ECHO.
+ECHO.
+ECHO cygwin installation updated
+ECHO  - %PACKAGES%
+ECHO.
+
+ENDLOCAL
+
+PAUSE
+EXIT /B 0
